290ac84fba2d423e0b591afaf8fad92d0da9db8d
PedrohBarbos <pedrohenbarbos@gmail.com>
2026-06-22 22:54:33 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+function Home () {
+    return (
+        <>
+         <div>
+                <h1>Olá Mãe</h1>
+                <p>Te amo</p>
+            </div>
+            <style jsx>{`
+                div {
+                    text-align: center;
+                    margin-top: 50px;
+                }
+                h1 {
+                    color: #333;
+                }
+                p {
+                    color: #666;
+                    font-size: 20px; 
+                }
+            `}</style>
+        </>
+    )
+}
+
+export default Home;
