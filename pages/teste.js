6da055671313a55a3ff67dc4cf400eb00a8f9b70
PedrohBarbos <pedrohenbarbos@gmail.com>
2026-06-28 00:40:15 +0000

    teste para o prettier

---
+++ pages/teste.js
@@ -0,0 +1,13 @@
+function Home() {
+  return <h1>Olá Mãe, eu te amo</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indenção errada");
+}
+
+export default Home;
