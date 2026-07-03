51be0671549ad17fc0ff4e107145acc65aa63c16
PedrohBarbos <pedrohenbarbos@gmail.com>
2026-07-03 22:05:31 +0000

    adiciona  endpoint

--- pages/teste.js
+++ pages/teste.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Olá Mãe, eu te amo</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indenção errada");
-}
-
 export default Home;
