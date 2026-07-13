18b9357270e797f1e3f38e253a5a6752bc40e73d
PedrohBarbos <pedrohenbarbos@gmail.com>
2026-07-13 22:47:31 +0000

    aa

--- Testes/integration/api/v1/status/get.test.js
+++ Testes/integration/api/v1/status/get.test.js
@@ -1,13 +1,17 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+import database from "infra/database.js";
+
+beforeAll(clearDatabase);
+
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
 
-  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toEqual(parsedUpdateAt);
-
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
