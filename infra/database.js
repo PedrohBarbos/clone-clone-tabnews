d106801811d19fc84f6d84f7c111669fc38e78e9
PedrohBarbos <pedrohenbarbos@gmail.com>
2026-07-23 23:43:49 +0000

    fix: resolves the client issue

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,5 @@
 import { Client } from "pg";
 
-console.log({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-});
-
 async function query(queryObject) {
   let client;
   try {
