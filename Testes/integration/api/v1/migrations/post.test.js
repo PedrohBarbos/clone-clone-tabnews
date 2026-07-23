5f2115cf697919fb835a55c0cee5fd26185dda47
PedrohBarbos <pedrohenbarbos@gmail.com>
2026-07-23 21:39:06 +0000

    revert: re-adds `response2` to `post.test.js`

--- Testes/integration/api/v1/migrations/post.test.js
+++ Testes/integration/api/v1/migrations/post.test.js
@@ -16,4 +16,14 @@ test("POST to /api/v1/migrations should return 200", async () => {
 
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
+
+  const responce2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(responce2.status).toBe(200);
+
+  const response2Body = await responce2.json();
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
 });
